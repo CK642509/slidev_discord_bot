909a309dd26e9154d6d0aa146a1db207f1412250
johnlin <john830110@gmail.com>
2025-04-24 22:29:45 +0800

    feat: 調整 layout 並重新命名

---
+++ layouts/TwoCols57.vue
@@ -0,0 +1,60 @@
+<!--
+  Usage:
+```md
+---
+layout: two-cols
+---
+# Left
+This shows on the left
+::right::
+# Right
+This shows on the right
+::bottom::
+This shows at the bottom, aligned to the end (bottom) of the grid
+```
+-->
+
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+  layoutClass: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <div class="slidev-layout two-cols w-full h-full" :class="layoutClass">
+    <div class="col-left" :class="props.class">
+      <slot />
+    </div>
+    <div class="col-right" :class="props.class">
+      <slot name="right" />
+    </div>
+    <div class="col-bottom" :class="props.class">
+      <slot name="bottom" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.two-cols {
+  display: grid;
+  /* grid-template-columns: repeat(2, 1fr); */
+  grid-template-columns: 5fr 7fr;
+  grid-template-rows: repeat(2, 1fr);
+}
+
+.col-left {
+  grid-area: 2 / 1 / 3 / 2;
+}
+.col-right {
+  grid-area: 2 / 2 / 3 / 3;
+}
+.col-bottom {
+  align-self: end;
+  grid-area: 3 / 1 / 3 / 3;
+}
+</style>
